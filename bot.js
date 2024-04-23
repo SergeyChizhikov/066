4c8339499b6c4e2561bee16a01d2aed6552e43fa
SergeyChizhikov <132288129+SergeyChizhikov@users.noreply.github.com>
2024-04-23 15:33:51 +0300

    Ходит не далее 4 страницы, переходит на главную

--- bot.js
+++ bot.js
@@ -11,23 +11,47 @@
 let bingInput = document.getElementsByName("q")[0];
 let bingBtn = document.getElementsByClassName("search")[0];
 let links = document.links;
-let keywords = ["Базовые вещи про GIT", "10 самых популярных шрифтов от Google",
-                "Отключение редакций и ревизий в WordPress", "Vite — отличное решение для проектов"];
-let keyword = keywords[getRandom(0, keywords.length)];
-
+//let keywords = ["Базовые вещи про GIT", "10 самых популярных шрифтов от Google",
+//                "Отключение редакций и ревизий в WordPress", "Vite — отличное решение для проектов"];
+//let keyword = keywords[getRandom(0, keywords.length)];
+let keyword = "Vite — отличное решение для проектов";
+//Работаем на главной странице
 if (bingBtn !== undefined) {
-  bingInput.value = keyword;
-  bingBtn.click();
-} else {
-  for (let i = 0; i < links.length; i++) {
-    if (links[i].href.indexOf("napli.ru") != -1) {
-      let link = links[i];
-      console.log("Нашел строку " + link);
-      link.click();
-      break;
+  let i = 0;
+  let timerId = setInterval(() => {
+    bingInput.value += keyword[i];
+    i++;
+    if (i == keyword.length) {
+      clearInterval(timerId);
+      bingBtn.click();
+    }
+  }, 500)
+  //Работаем на странице поисковой выдачи
+  } else if (document.querySelector(".b_scopebar") !== null){
+    let nextPage = true;
+    for (let i = 0; i < links.length; i++) {
+      if (links[i].href.indexOf("napli.ru") != -1) {
+        let link = links[i];
+        let nextPage = false;
+        console.log("Нашел строку " + link);
+        setTimeout(() => {
+          link.click();
+        }, getRandom(2000, 3000));
+        break;
+      }
+    }
+    if (document.querySelector(".sb_pagS").innerText == "4") {
+      let nextPage = false;
+      location.href = "https://www.bing.com/"
+    }
+
+    if (nextPage) {
+      setTimeout(() => {
+        document.querySelector(".sb_pagN").click();
+      }, getRandom(2500, 3500))
+
     }
   }
-}
 
 function getRandom(min,max) {
   return Math.floor(Math.random() * (max - min) + min);
