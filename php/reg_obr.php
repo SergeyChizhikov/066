941d6074e3a990b49fce3fa01c586fe78874dd8b
SergeyChizhikov <132288129+SergeyChizhikov@users.noreply.github.com>
2024-05-02 16:51:23 +0300

    Добавлена асин-я ф-я в форму рег-ии

--- php/reg_obr.php
+++ php/reg_obr.php
@@ -12,13 +12,21 @@ $mysqli = mysqli_connect($host, $user, $password, $db);
 if ($mysqli == false) {
       print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
 } else {
-      print("Соединение установлено успешно");
+      //print("Соединение установлено успешно");
       $name = $_POST["name"];
       $lastname = $_POST["lastname"];
       $email = $_POST["email"];
       $pass = $_POST["pass"];
 
-      $mysqli->query("INSERT INTO `users`(`name`, `lastname`, `email`, `pass`) VALUES ('$name', '$lastname', '$email', '$pass')");
+      $result = $mysqli->query("SELECT * FROM `users` WHERE `email`='$email'");
+
+      if ($result->num_rows != 0) {
+            print("Такой пользователь уже существует");
+      } else {
+            $mysqli->query("INSERT INTO `users`(`name`, `lastname`, `email`, `pass`) VALUES ('$name', '$lastname', '$email', '$pass')");
+            print("Пользователь успешно зарегистрирован!");
+      }
+
 
       // $sql = "INSERT INTO `users`(`name`, `lastname`, `email`, `pass`) VALUES (?,?,?,?)";
       // $stmt = $mysqli->prepare($sql);
