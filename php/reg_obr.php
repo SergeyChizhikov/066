48edb6b774ebeb2108982263db31fecd461ac60b
SergeyChizhikov <132288129+SergeyChizhikov@users.noreply.github.com>
2024-05-03 17:02:11 +0300

    Добавил авторизацию и лк

--- php/reg_obr.php
+++ php/reg_obr.php
@@ -12,13 +12,14 @@ $mysqli = mysqli_connect($host, $user, $password, $db);
 if ($mysqli == false) {
       print("error");
 } else {
-      //print("Соединение установлено успешно");
+
       $name = $_POST["name"];
       $lastname = $_POST["lastname"];
-      $email = $_POST["email"];
-      $pass = $_POST["pass"];
+      $email = trim(mb_strtolower($_POST["email"]));
+      $pass = trim($_POST["pass"]);
+      $pass = password_hash($pass, PASSWORD_DEFAULT);
 
-      $result = $mysqli->query("SELECT * FROM `users` WHERE `email`='$email' AND `pass`='$pass'");
+      $result = $mysqli->query("SELECT * FROM `users` WHERE `email`='$email'");
 
       if ($result->num_rows != 0) {
             print("exist");
